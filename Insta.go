4872b65b46528f05479a21205bdb38877b5671b1
Harshavardhan <Harshavardhanjo@gmail.com>
2021-10-09 14:06:09 +0530

    first

---
+++ Insta.go
@@ -0,0 +1,217 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+)
+
+var client *mongo.Client
+
+type Person struct {
+	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Firstname string             `json:"firstname,omitempty" bson:"firstname,omitempty"`
+	Lastname  string             `json:"lastname,omitempty" bson:"lastname,omitempty"`
+}
+
+type User struct {
+	ID       primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Username string             `json:"Username,omitempty" bson:"Username,omitempty"`
+	Password string             `json:"Password,omitempty" bson:"Password,omitempty"`
+	Email    string             `json:"Email,omitempty" bson:"Email,omitempty"`
+}
+
+type Post struct {
+	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Caption   string             `json:"Caption,omitempty" bson:"Caption,omitempty"`
+	URL       string             `json:"url,omitempty" bson:"url,omitempty"`
+	Timestamp string             `json:"Timestamp,omitempty" bson:"Timestamp,omitempty"`
+	UserId    string             `json:"userid,omitempty" bson:"userid,omitempty"`
+}
+
+func Encrypt(key, data []byte) ([]byte, error) {
+	blockCipher, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(blockCipher)
+	if err != nil {
+		return nil, err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err = rand.Read(nonce); err != nil {
+		return nil, err
+	}
+
+	ciphertext := gcm.Seal(nonce, nonce, data, nil)
+
+	return ciphertext, nil
+}
+
+func Decrypt(key, data []byte) ([]byte, error) {
+	blockCipher, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(blockCipher)
+	if err != nil {
+		return nil, err
+	}
+
+	nonce, ciphertext := data[:gcm.NonceSize()], data[gcm.NonceSize():]
+
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return plaintext, nil
+}
+
+func GenerateKey() ([]byte, error) {
+	key := make([]byte, 32)
+
+	_, err := rand.Read(key)
+	if err != nil {
+		return nil, err
+	}
+
+	return key, nil
+}
+
+func CreateUserEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	var user User
+	_ = json.NewDecoder(request.Body).Decode(&user)
+	collection := client.Database("Insta").Collection("User")
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	key, err := GenerateKey()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ciphertext, err := Encrypt(key, []byte(user.Password))
+	if err != nil {
+		log.Fatal(err)
+	}
+	user.Password = string(ciphertext)
+	result, _ := collection.InsertOne(ctx, user)
+	json.NewEncoder(response).Encode(result)
+}
+
+func GetUserEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	params := mux.Vars(request)
+	id, _ := primitive.ObjectIDFromHex(params["id"])
+	var user User
+	collection := client.Database("Insta").Collection("User")
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	err := collection.FindOne(ctx, User{ID: id}).Decode(&user)
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	json.NewEncoder(response).Encode(user)
+}
+
+func GetPostEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	params := mux.Vars(request)
+	id, _ := primitive.ObjectIDFromHex(params["id"])
+	var post Post
+	collection := client.Database("Insta").Collection("Post")
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	err := collection.FindOne(ctx, Post{ID: id}).Decode(&post)
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	json.NewEncoder(response).Encode(post)
+}
+
+// func GetPostsEndpoint(response http.ResponseWriter, request *http.Request) {
+// 	response.Header().Set("content-type", "application/json")
+// 	params := mux.Vars(request)
+// 	id, _ := params["id"]
+// 	var posts []Post
+// 	collection := client.Database("Insta").Collection("Post")
+// 	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+// 	err := collection.Find(ctx, bson.M{"UserId" : id}).Decode(&post)
+// 	if err != nil {
+// 		response.WriteHeader(http.StatusInternalServerError)
+// 		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+// 		return
+// 	}
+// 	json.NewEncoder(response).Encode(post)
+// }
+
+func GetPostsEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	params := mux.Vars(request)
+	id, _ := params["userid"]
+	var posts []Post
+	collection := client.Database("Insta").Collection("Post")
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	cursor, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	defer cursor.Close(ctx)
+	for cursor.Next(ctx) {
+		var post Post
+		cursor.Decode(&post)
+		if post.UserId == id {
+			posts = append(posts, post)
+		}
+	}
+	if err := cursor.Err(); err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	json.NewEncoder(response).Encode(posts)
+}
+
+func CreatePostEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	var post Post
+	_ = json.NewDecoder(request.Body).Decode(&post)
+	collection := client.Database("Insta").Collection("Post")
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	result, _ := collection.InsertOne(ctx, post)
+	json.NewEncoder(response).Encode(result)
+}
+
+func main() {
+	fmt.Println("Starting the application...")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	clientOptions := options.Client().ApplyURI("mongodb+srv://Harsha:sathyajothi@cluster0.igo6i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+	client, _ = mongo.Connect(ctx, clientOptions)
+	router := mux.NewRouter()
+	router.HandleFunc("/users", CreateUserEndpoint).Methods("POST")
+	router.HandleFunc("/posts", CreatePostEndpoint).Methods("POST")
+	router.HandleFunc("/user/{id}", GetUserEndpoint).Methods("GET")
+	router.HandleFunc("/posts/{id}", GetPostEndpoint).Methods("GET")
+	router.HandleFunc("/posts/users/{userid}", GetPostsEndpoint).Methods("GET")
+	http.ListenAndServe(":12345", router)
+}
