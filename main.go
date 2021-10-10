acc211c41f083d70baae2cf5b4f555954d3335ce
Harshavardhan <Harshavardhanjo@gmail.com>
2021-10-10 13:18:12 +0530

    final

--- main.go
+++ main.go
@@ -24,12 +24,6 @@ import (
 
 var client *mongo.Client
 
-type Person struct {
-	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	Firstname string             `json:"firstname,omitempty" bson:"firstname,omitempty"`
-	Lastname  string             `json:"lastname,omitempty" bson:"lastname,omitempty"`
-}
-
 type User struct {
 	ID       primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	Username string             `json:"Username,omitempty" bson:"Username,omitempty"`
